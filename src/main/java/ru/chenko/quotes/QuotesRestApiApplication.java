fbaa2e6ccc85a62ac6255b223bd701a439712bd5
chenko16 <Durachenko-serg@yandex.ru>
2020-04-11 20:40:10 +0300

    Добавил плагины maven для сборки jar

--- src/main/java/ru/chenko/quotes/QuotesRestApiApplication.java
+++ src/main/java/ru/chenko/quotes/QuotesRestApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecuritiesRestApiApplication {
+public class QuotesRestApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecuritiesRestApiApplication.class, args);
+		SpringApplication.run(QuotesRestApiApplication.class, args);
 	}
 
 }
