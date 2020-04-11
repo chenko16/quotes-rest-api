02e40f772a9bb7ef5d9130b03bf7d2786c00da64
chenko16 <Durachenko-serg@yandex.ru>
2020-04-11 13:32:16 +0300

    Init

---
+++ src/main/java/ru/chenko/securities/SecuritiesRestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.chenko.securities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritiesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritiesRestApiApplication.class, args);
+	}
+
+}
